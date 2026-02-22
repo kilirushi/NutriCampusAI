c32167630bdd2e98ff605a58a43d010a24e9a295
Erica Edem Kankoe <kankoeericaedem@gmail.com>
2026-02-22 07:10:28 -0500

    commit code

--- source/backend/nutri-campus-ai/src/app.js
+++ source/backend/nutri-campus-ai/src/app.js
@@ -1,17 +1,27 @@
 const express = require("express");
 const path = require("path");
 const mealplanRoutes = require("./routes/mealplan.route");
+const mealplanService = require("./services/mealplan.service");
 
 const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "view"));
 
-app.use(express.json());
+app.use(express.static(path.join(__dirname, "view")));
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/", async (req, res) => {
+    try {
+        const topDishes = await mealplanService.getTopDishes();
+        console.log(topDishes);
+        res.render("index", { topDishes });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Server Error");
+    }
 });
 
 app.use("/api/mealplan", mealplanRoutes);
+
 module.exports = app;
