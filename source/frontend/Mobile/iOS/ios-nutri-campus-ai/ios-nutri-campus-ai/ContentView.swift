0f32e7aa17243d181a397ff272de88b3336fcac0
Hoà Trần <tranvanhoahndt@gmail.com>
2026-02-21 20:46:16 -0500

    create project

---
+++ source/frontend/Mobile/iOS/ios-nutri-campus-ai/ios-nutri-campus-ai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios-nutri-campus-ai
+//
+//  Created by Trần Hoà on 21/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ source/frontend/Mobile/iOS/ios-nutri-campus-ai/ios-nutri-campus-ai/ios_nutri_campus_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_nutri_campus_aiApp.swift
+//  ios-nutri-campus-ai
+//
+//  Created by Trần Hoà on 21/2/26.
+//
+
+import SwiftUI
+
+@main
+struct ios_nutri_campus_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
