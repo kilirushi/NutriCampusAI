b3615f676947eaee41fb91d24bff35af1a2bf459
sonhoang002 <120953254+sonhoang002@users.noreply.github.com>
2026-02-22 07:45:06 -0500

    Change how the admin board look

--- source/backend/nutri-campus-ai/src/app.js
+++ source/backend/nutri-campus-ai/src/app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const path = require("path");
 const mealplanRoutes = require("./routes/mealplan.route");
 const mealplanService = require("./services/mealplan.service");
 
@@ -12,18 +11,23 @@ app.set("views", path.join(__dirname, "view"));
 app.use(express.static(path.join(__dirname, "view")));
 
 app.get("/", async (req, res) => {
-    try {
-        const topDishes = await mealplanService.getTopDishes();
-        console.log(topDishes);
-        res.render("index", { topDishes });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Server Error");
-    }
+  try {
+    const topDishes = await mealplanService.getTopDishes();
+    const bottomDishes = await mealplanService.getDislikedDishes();
+    const bottomDishesWeekly = await mealplanService.getDislikedDishesWeek();
+    const topDishesWeekly = await mealplanService.getTopDishesWeek();
+    res.render("index", {
+      topDishes,
+      bottomDishes,
+      topDishesWeekly,
+      bottomDishesWeekly,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Server Error");
+  }
 });
 
 app.use("/api/mealplan", mealplanRoutes);
-
 
 module.exports = app;
